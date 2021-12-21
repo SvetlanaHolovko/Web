a2093c19f52cd8e4ae4e6f2173e9be7708f302e3
SvetlanaHolovko <svetta.gsn@gmail.com>
2021-12-21 17:34:25 +0200

    Added slider

--- js/app.js
+++ js/app.js
@@ -59,8 +59,8 @@ if (burgerMenu) {
 /*========== ПЛАН ===========*/
 /*
 1. Сверстать слайдер - готово
-2. Сделать переключение фотографий влево и вправо
-3. Сделать переключение фото по клику на нижнее фото
+2. Сделать переключение фотографий влево и вправо - готово
+3. Сделать переключение фото по клику на нижнее фото - готово
 4. При клике на большую картинку - увеличивать картинку
 */
 
@@ -76,7 +76,7 @@ var images = [
 ];
 
 // Путь к картинке слайдера
-var path = "img/works/";
+var path = "img/sliders/";
 // Текущая картинка
 var currentImage = 0;
 
@@ -84,7 +84,7 @@ var currentImage = 0;
 $("#main-slider img").attr("src", path + images[currentImage]);
 
 // Клик по правой стрелке
-$("#main-slider .team__next").on('click', function () {
+$("#main-slider .slider__content-next").on('click', function () {
 	// при клике увеличиваем на 1
 	currentImage++;
 	if (currentImage >= images.length) {
@@ -95,15 +95,12 @@ $("#main-slider .team__next").on('click', function () {
 	$('#slides ul li').removeClass('active');
 	// Присваиваем класс active у текущей нижней картинки		
 	$("#slides ul li[data-id = '" + currentImage + "'] ").addClass("active");
-
-
 });
 
 // Клик по левой стрелке
-$("#main-slider .team__prev").on('click', function () {
+$("#main-slider .slider__content-prev").on('click', function () {
 	// при клике уменьшаем на 1
 	currentImage--;
-
 	if (currentImage < 0 || currentImage >= images.length) {
 		currentImage = images.length - 1;
 	}
@@ -114,31 +111,28 @@ $("#main-slider .team__prev").on('click', function () {
 	$("#slides ul li[data-id = '" + currentImage + "'] ").addClass("active");
 });
 
-// // Создание карточек фотографий
-// for (var i = 0; i < images.length; i++) {
-// 	// Добавляем элемент в блок с мини картинками
-// 	$('#slides ul').append("<li data-id='" + i + "'>" + 
-// 		"<img src='" + 
-// 		path + images[i] + 
-// 		"'></li>");
-// 	// Проверка - если это первая картинка, то добавляем класс active
-// 	if (i == 0) {
-// 		$('#slides ul li').addClass('active');
-// 	}
-// }
+// Создание карточек фотографий
+for (var i = 0; i < images.length; i++) {
+	// Добавляем элемент в блок с мини картинками
+	$('#slides ul').append("<li data-id='" + i + "'>" +
+		"<img src='" + path + images[i] + "'></li>");
+	// Проверка - если это первая картинка, то добавляем класс active
+	if (i == 0) {
+		$('#slides ul li').addClass('active');
+	}
+}
 
-// // Делаем клик по слайдам
-// $('#slides ul li').on('click', function(e) {
-// 	// Убираем у всех элементов класс active
-// 	$('#slides ul li').removeClass('active');
-// 	// На элементе по которому кликнули добавляем класс active
-// 	$(this).addClass('active');
-// 	// Получаем id элемента по которому кликнули
-// 	var id = this.dataset.id;
-
-// 	// Меняем картинку в слайдере
-// 	$("#main-slider img").attr("src", path + images[id]);
-// });
+// Делаем клик по слайдам
+$('#slides ul li').on('click', function (e) {
+	// Убираем у всех элементов класс active
+	$('#slides ul li').removeClass('active');
+	// На элементе по которому кликнули добавляем класс active
+	$(this).addClass('active');
+	// Получаем id элемента по которому кликнули
+	var id = this.dataset.id;
+	// Меняем картинку в слайдере
+	$("#main-slider img").attr("src", path + images[id]);
+});
 
 // Увеличиваем картинку
 $("#main-slider img").on('click', function () {
@@ -148,6 +142,7 @@ $("#main-slider img").on('click', function () {
 		.css({ 'display': 'block' })
 		.append('<img src="' + $(this).attr('src') + '">');
 });
+
 // Уменьшаем картинку
 $("#opacity").on('click', function () {
 	$('#opacity').css({ 'display': 'none' });
--- js/main.js
+++ js/main.js
@@ -18,79 +18,84 @@ var images = [
 ];
 
 // Путь к картинке слайдера
-var path = "img/works/";
+var path = "img/sliders/";
 // Текущая картинка
-var currentImage = 0;
+//var currentImage = 0;
 
 // Первая картинка
-$("#maine-slider img").attr("src", path + images[currentImage]);
+$("#maine-slider img")
+	.attr("src", path + images[0]);
+//.css({ "border": "20px solid #000" });
+
+console.dir($("#main-slider img"))
 // Перелистываем вправо next
-$("#maine-slider .next").click(function() {
-	// при клике увеличиваем на 1
-	currentImage++;
-	if(currentImage >= images.length) {
-		currentImage = 0;
-	}
+//$("#maine-slider .slider__content-next").click(function () {
+//	// при клике увеличиваем на 1
+//	currentImage++;
+//	if (currentImage >= images.length) {
+//		currentImage = 0;
+//	}
 
-	$("#maine-slider img").attr("src", path + images[currentImage]);
+//	$("#maine-slider img").attr("src", path + images[currentImage]);
 
-	// Убираем класс active у всех нижних картинок
-	$('#slides ul li').removeClass('active');
-	// Присваиваем класс active у текущей нижней картинки		
-	$("#slides ul li[data-id = '" + currentImage + "'] ").addClass("active");
-});
+//	// Убираем класс active у всех нижних картинок
+//	$('#slides ul li').removeClass('active');
+//	// Присваиваем класс active у текущей нижней картинки
+//	$("#slides ul li[data-id = '" + currentImage + "'] ").addClass("active");
+//});
 
-// Перелистываем влево pref
-$("#maine-slider .pref").click(function() {
-	// при клике уменьшаем на 1
-	currentImage--;
+//// Перелистываем влево pref
+//$("#maine-slider .pref").click(function() {
+//	// при клике уменьшаем на 1
+//	currentImage--;
 
-	if(currentImage < 0 || currentImage >= images.length) {
-		currentImage = images.length-1;
-	}
+//	if(currentImage < 0 || currentImage >= images.length) {
+//		currentImage = images.length-1;
+//	}
 
-	$("#maine-slider img").attr("src", path + images[currentImage]);
-	// Убираем класс active у всех нижних картинок
-	$('#slides ul li').removeClass('active');
-	// Присваиваем класс active у текущей нижней картинки		
-	$("#slides ul li[data-id = '" + currentImage + "'] ").addClass("active");
-});
+//	$("#maine-slider img").attr("src", path + images[currentImage]);
+//	// Убираем класс active у всех нижних картинок
+//	$('#slides ul li').removeClass('active');
+//	// Присваиваем класс active у текущей нижней картинки
+//	$("#slides ul li[data-id = '" + currentImage + "'] ").addClass("active");
+//});
 
-// Создание карточек фотографий
-for (var i = 0; i < images.length; i++) {
-	// Добавляем элемент в блок с мини картинками
-	$('#slides ul').append("<li data-id='" + i + "'>" + 
-		"<img src='" + 
-		path + images[i] + 
-		"'></li>");
-	// Проверка - если это первая картинка, то добавляем класс active
-	if (i == 0) {
-		$('#slides ul li').addClass('active');
-	}
-}
+//// Создание карточек фотографий
+//for (var i = 0; i < images.length; i++) {
+//	// Добавляем элемент в блок с мини картинками
+//	$('#slides ul').append("<li data-id='" + i + "'>" +
+//		"<img src='" +
+//		path + images[i] +
+//		"'></li>");
+//	// Проверка - если это первая картинка, то добавляем класс active
+//	if (i == 0) {
+//		$('#slides ul li').addClass('active');
+//	}
+//}
 
-// Делаем клик по слайдам
-$('#slides ul li').on('click', function(e) {
-	// Убираем у всех элементов класс active
-	$('#slides ul li').removeClass('active');
-	// На элементе по которому кликнули добавляем класс active
-	$(this).addClass('active');
-	// Получаем id элемента по которому кликнули
-	var id = this.dataset.id;
+//// Делаем клик по слайдам
+//$('#slides ul li').on('click', function(e) {
+//	// Убираем у всех элементов класс active
+//	$('#slides ul li').removeClass('active');
+//	// На элементе по которому кликнули добавляем класс active
+//	$(this).addClass('active');
+//	// Получаем id элемента по которому кликнули
+//	var id = this.dataset.id;
 
-	// Меняем картинку в слайдере
-	$("#maine-slider img").attr("src", path + images[id]);
-});
+//	// Меняем картинку в слайдере
+//	$("#maine-slider img").attr("src", path + images[id]);
+//});
 
-// Увеличиваем картинку
-$("#maine-slider img").on('click', function() {
-	$('#opacity').css({'display':'block'});
-	$('#full-image')
-		.css({'display':'block'})
-		.append('<img src="' + $(this).attr('src') + '">');
-});
-// Уменьшаем картинку
-$("#opacity").on('click', function() {
-	$('#opacity').css({'display':'none'});
-	$('#full-image').css({'display':'none'}).empty();
-});
+//// Увеличиваем картинку
+//$("#maine-slider img").on('click', function() {
+//	$('#opacity').css({'display':'block'});
+//	$('#full-image')
+//		.css({'display':'block'})
+//		.append('<img src="' + $(this).attr('src') + '">');
+//});
+
+//// Уменьшаем картинку
+//$("#opacity").on('click', function() {
+//	$('#opacity').css({'display':'none'});
+//	$('#full-image').css({'display':'none'}).empty();
+//});
